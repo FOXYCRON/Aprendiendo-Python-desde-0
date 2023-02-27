c27686b792e0c3bc8b81d6776680765e8edd6612
FOXYCRON <irvinadrian5@gmail.com>
2023-02-26 23:20:57 -0700

    inputs

--- inputs/inputs_numeros.py
+++ inputs/inputs_numeros.py
@@ -2,7 +2,13 @@
 numero = input("Dame un numero: ")
 
 # Convierto el numero a entero y lo multiplico por 2
-resultado = int(numero) * 2
+resultado_entero = int(numero) * 2
+
+# Convierto el numero a flotante y lo multiplico por 2
+resultado_flotante = float(numero) * 2
+
+# Si el resultado es flotante lo convierte en entero y lo envia
+resultadode_entero_a_flotante = int(float(numero) * 2)
 
 # Mostrando el resultado
-print(resultado)
+print(resultado_entero)
